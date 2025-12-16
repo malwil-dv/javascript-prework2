144940c9043c65e960cf035611f7aa5a55631151
Malgorzata Wilk <pocztamwilk@gmail.com>
2025-12-16 20:49:09 +0100

    Add game rules

--- js/script.js
+++ js/script.js
@@ -29,3 +29,19 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ja wygrywam!');
+} else if (computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Ja wygrywam!');
+} else if (computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ja wygrywam!');
+} else if (computerMove == playerMove){
+    printMessage('Remis');
+}
